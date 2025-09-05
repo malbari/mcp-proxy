1c274996005f1682ccc300555d988826e563c717
Maurizio Albari <maurizio.albari@gmail.com>
2025-09-05 08:28:44 +0200

    removed bash

--- start-turismo.sh
+++
@@ -1,6 +0,0 @@
-docker run -d --restart unless-stopped -it \
-  -p 8080:8080 \
-  mcp-proxy-node --port 8080 --host 0.0.0.0 -- \
-  npx -y "@tyk-technologies/api-to-mcp@latest" \
-  --spec "https://raw.githubusercontent.com/APT-Servizi/openapi-ert/main/v1/api-spec.yaml" \
-  --targetUrl "https://emiliaromagnaturismo.it/opendata/v1"
